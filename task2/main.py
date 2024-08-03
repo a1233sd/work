acefd748603e91447733866f2780b3bd0dadf29a
vladimir <75137066+a1233sd@users.noreply.github.com>
2024-08-03 20:00:42 +0300

    Add files via upload

--- task2/main.py
+++ task2/main.py
@@ -41,6 +41,8 @@ def build_mna_matrix(elements):
     node_index = {node: i for i, node in enumerate(node_list)}
 
     num_nodes = len(node_list)
+    num_voltage_sources = len(voltage_sources)
+
     G = np.zeros((num_nodes, num_nodes))
     I = np.zeros(num_nodes)
 
@@ -59,12 +61,25 @@ def build_mna_matrix(elements):
             G[n1][n1] += 1 / resistance
 
     for (node1, node2, voltage) in voltage_sources:
-        if node1 != 'gnd':
-            n1 = node_index[node1]
-            I[n1] += voltage
-        if node2 != 'gnd':
+        if node1 == 'gnd':
             n2 = node_index[node2]
             I[n2] -= voltage
+        elif node2 == 'gnd':
+            n1 = node_index[node1]
+            I[n1] += voltage
+        else:
+            n1, n2 = node_index[node1], node_index[node2]
+            G = np.pad(G, ((0, 1), (0, 1)), mode='constant')
+            I = np.pad(I, (0, 1), mode='constant')
+            G[n1][-1] = 1
+            G[-1][n1] = 1
+            G[n2][-1] = -1
+            G[-1][n2] = -1
+            I[-1] = voltage
+
+    # Удаление последней строки и столбца, если они добавлены для источников напряжения
+    G = G[:num_nodes, :num_nodes]
+    I = I[:num_nodes]
 
     return G, I, node_index
 
@@ -86,6 +101,13 @@ def main():
 
     elements = parse_input_file(input_file)
     G, I, node_index = build_mna_matrix(elements)
+
+    # Проверка матрицы G и вектора I перед решением
+    print("Матрица G:")
+    print(G)
+    print("Вектор I:")
+    print(I)
+
     V = solve_mna(G, I)
 
     node_voltages = {node: V[index] for node, index in node_index.items()}
